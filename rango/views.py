7935717510ed5b37f1572cfec789426caeb65ae3
Rapteon <rapteon@tutanota.com>
2025-01-20 15:27:49 +0000

    Exercise from page 40 of the book.

--- rango/views.py
+++ rango/views.py
@@ -6,4 +6,12 @@ from django.http import HttpResponse
 # more functions in this file. Each view requires a HttpRequest object
 # as one argument and must return a HttpResponse object.
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse(
+        'Rango says hey there partner!<br><a href="/rango/about/">About</a>'
+    )
+
+
+def about(request):
+    return HttpResponse(
+        'Rango says here is the about page.<br><a href="/rango/">Index</a>'
+    )
