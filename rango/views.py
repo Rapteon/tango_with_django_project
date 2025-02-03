474a4754ef8bd8f94a98157e10e44dca780a4c6d
Rapteon <rapteon@tutanota.com>
2025-02-03 10:33:17 +0000

    Added template for About page and also added cat image for media file example.

--- rango/views.py
+++ rango/views.py
@@ -15,6 +15,5 @@ def index(request):
 
 
 def about(request):
-    return HttpResponse(
-        'Rango says here is the about page.<br><a href="/rango/">Index</a>'
-    )
+    context_dict = {"boldmessage": "This tutorial has been put together by Rapteon."}
+    return render(request, "rango/about.html", context=context_dict)
